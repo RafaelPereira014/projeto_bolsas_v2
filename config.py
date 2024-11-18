ab5d88f016a062234733304ed066b52275c6253e
RafaelPereira014 <rafaelbotelhopereira@ua.pt>
2024-11-18 14:35:05 -0100

    changes

--- config.py
+++
@@ -1,7 +0,0 @@
-db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'passroot',
-    'db': 'bolsas_ilha_v2',
-    'charset': 'utf8mb4',  # Use utf8mb4 for full Unicode support
-}
